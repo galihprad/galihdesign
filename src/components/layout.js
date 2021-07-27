220fb48e0a23c2415592e97553a88df74c384f80
galihprad <galih.p.w.j@gmail.com>
2021-07-27 11:24:35 +0700

    Update styling

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,9 +25,8 @@ const Layout = ({ location, title, children }) => {
       <header className="global-header">{header}</header>
       <main>{children}</main>
       <footer>
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.com">Gatsby</a>
+        thanks to
+        <a href="https://www.gatsbyjs.com"> GatsbyJS</a>
       </footer>
     </div>
   )
